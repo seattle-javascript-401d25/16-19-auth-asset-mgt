95eb8bd4b4e5d944e76d4559919476444d81fade
Devin Cunningham <dtyler.cunningham@gmail.com>
2018-07-02 13:15:26 -0700

    on to whiteboarding

---
+++ src/__test__/lib/account-mock.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import faker from 'faker';
+import Account from '../../model/account';
+import { access } from 'fs';
+
+const pCreateAccountMock = () => {
+  const mockData = {};
+  const originalReq = {
+    username: faker.internet.username(),
+    email: faker.internet.email(),
+    password: faker.lorem.word(8),
+  };
+
+  return Account.create(originalReq.username, originalReq.email, originalReq.password)
+  .then((account) => {
+    mockData.originalReq = originalReq;
+    mockData.account = account;
+    return account.pCreateToken();
+  })
+  .then((token) => {
+    mockData.token = token;
+    return Account.findById(mockData.account._id);
+  })
+  .then((account) => {
+    mockData.account = account;
+    return mockData;
+  });
+};
+
+const pRemoveAccountMock = () => Account.remove({});
