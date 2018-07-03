a2313bd5f97810be96c1bfdb1dc7705b60100f3f
Noah Israel <noahmvf@gmail.com>
2018-07-03 14:41:48 -0700

    tests 409 and 400 added

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 https://travis-ci.org/noahmvf/16-19-auth-asset-mgt
 
+List all of your registered routes and describe their behavior. 
+
+Describe what your resouce is. Imagine you are providing this API to other developers who need to research your API in order to use it. 
+
+Describe how a developer should be able to make requests to your API. Refer to the PokeAPI docs for a good example to follow.
+
 
--- src/__test__/auth-router.test.js
+++ src/__test__/auth-router.test.js
@@ -14,6 +14,7 @@ describe('AUTH router', () => {
   afterAll(stopServer);
   afterEach(removeAccountMockPromise);
 
+  // POST REQUEST
   test('POST 200 to /api/signup for successful account creation and receipt of a TOKEN', () => {
     const mockAccount = {
       username: faker.internet.userName(),
@@ -31,11 +32,40 @@ describe('AUTH router', () => {
       });
   });
 
+  test('POST 400 for bad account request', () => {
+    const mockAccount = {
+      username: faker.internet.userName(),
+    };
+    return superagent.post(apiUrl)
+      .send(mockAccount)
+      .then((response) => {
+        throw response;
+      })
+      .catch((err) => {
+        expect(err.status).toEqual(400);
+      });
+  });
 
+  test('POST 409 for duplicate username', () => {
+    return createAccountMockPromise()
+      .then((mockData) => {
+        return superagent.post(`${apiUrl}/signup`)
+          .send({ username: mockData.account.username, email: mockData.account.email, password: '134566' });
+      })
+      .then((response) => {
+        throw response;
+      })
+      .catch((err) => {
+        expect(err.status).toEqual(409);
+      });
+  });
+
+
+  // GET REQUEST
   test('GET 200 to api/login for successful login and receipt of a TOKEN', () => {
     return createAccountMockPromise()
       .then((mockData) => {
-        // token = mockData.token; 
+      // token = mockData.token; 
         return superagent.get(`${apiUrl}/login`)
           .auth(mockData.account.username, mockData.originalRequest.password);
       })
@@ -59,15 +89,3 @@ describe('AUTH router', () => {
       });
   });
 });
-
-// test('GET 409 to /api/login for duplicate username', () => {
-//   return superagent.get(`${apiUrl}/login`)
-//     .auth('bad username', 'bad password')
-//     .then((response) => {
-//       throw response;
-//     })
-//     .catch((err) => {
-//       expect(err.status).toEqual(400);
-//     });
-// });
-
