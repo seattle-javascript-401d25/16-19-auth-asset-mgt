f8295c1c2ea184440fe58b3db9d0ebe122601c3c
Noah Israel <noahmvf@gmail.com>
2018-07-08 10:16:32 -0700

    .env, test.env, and .gitignore updated

--- src/__test__/lib/test.env.js
+++ src/__test__/lib/test.env.js
@@ -1,5 +1,16 @@
 process.env.NODE_ENV = 'development';
 process.env.PORT = 5000;
-process.env.MONGODB_URI = 'mongodb://localhost:27017/test';
+process.env.MONGODB_URI = 'mongodb://localhost/testing';
 
-process.env.SECRET_KEY = 'randomstdringasdfasf309484950495';
+process.env.SECRET_KEY = 'randomkey';
+
+const isAwsMock = true;
+
+if (isAwsMock) {
+  process.env.AWS_BUCKET = 'fake';
+  process.env.AWS_SECRET_ACCESS_KEY = 'fake';
+  process.env.AWS_ACCESSS_KEY_ID = 'fake';
+  require('./setup');
+} else {
+  require('dotenv').config();
+}
